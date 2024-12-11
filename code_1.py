3cae4f8447c175f71df85129a0239f0f6e7eead6
Silvia Santamaria <silviasantabuil@outlook.com>
2024-12-11 13:09:31 +0000

    no way i can click now and do other stuff

--- code_1.py
+++ code_1.py
@@ -12,6 +12,26 @@ emptyTextBoxColour = (38,32,54)
 emptyTextColour = (189,174,232)
 mySurface = None
 
+class buttonNextPage:
+  def __init__(self, boxcolour, textcolour, coords, text, page, surface, font):
+    self.boxcolour = boxcolour
+    self.textcolour = textcolour
+    self.coords = coords
+    self.text = text
+    self.page = page
+    self.surface = surface
+    self.font = font
+
+  def drawText(self):
+    buttonText = self.font.render(str(self.text), False, self.textcolour)
+    buttonRect = buttonText.get_rect()
+    buttonRect.topleft = (self.coords)
+    self.surface.blit(buttonText, buttonRect)
+  
+  def drawButton(self):
+    buttonBase = pygame.Rect(self.coords[0],self.coords[1],140,40)
+    pygame.draw.rect(self.surface,self.boxcolour,buttonBase)
+
 def initialise(windowWidth, windowHeight, windowName, windowColour):
   global mySurface
   pygame.init()
@@ -19,20 +39,45 @@ def initialise(windowWidth, windowHeight, windowName, windowColour):
   pygame.display.set_caption(windowName)
   mySurface.fill(color=windowColour)
 
+def events():
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+
 def render():
-  pygame.draw.rect(mySurface, emptyTextBoxColour, (55,100,250,40))
-  box1 = font.render('Email', True, emptyTextColour)
-  mySurface.blit(box1, (65,100))
-  pygame.draw.rect(mySurface, emptyTextBoxColour, (55,160,250,40))  
-  box2 = font.render('Password', True, emptyTextColour)
-  mySurface.blit(box2, (65,160))
-  pygame.display.update()
+  buttonnp.drawButton()
+  buttonnp.drawText()
+  # pygame.draw.rect(mySurface, emptyTextBoxColour, (55,100,250,40))
+  # box1 = font.render('Email', True, emptyTextColour)
+  # mySurface.blit(box1, (65,100))
+  # pygame.draw.rect(mySurface, emptyTextBoxColour, (55,160,250,40))  
+  # box2 = font.render('Password', True, emptyTextColour)
+  # mySurface.blit(box2, (65,160))
+  # pygame.display.update()
+
+down = False
+up = True
+clicked = False
 
 initialise(width, height, caption, bgColour)
+buttonnp = buttonNextPage((255,255,255), (255,0,0), (50,50), "hi", "page", mySurface, font)
 while True:
+  clicked = False
   render()
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
+  pos = pygame.mouse.get_pos()
+  buttons = pygame.mouse.get_pressed()
+  if up and buttons[0]:
+    up = False
+    down = True
+  elif (not buttons[0]) and down:
+    up = True
+    clicked = True
+    down = False
+#  print(buttons[0])
+#  print(up,down,clicked)
+  if clicked:
+    print("Clicked")
+  events()
+  pygame.display.flip()
 
--- tablepractice.py
+++ tablepractice.py
@@ -2,25 +2,18 @@ import sqlite3
 
 connection = sqlite3.connect('tablez.db')
 cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS TABLEZ")
+#cursor.execute("DROP TABLE IF EXISTS TABLEZ")
 table = """CREATE TABLE TABLEZ (
            Email VARCHAR(255) NOT NULL,
            First_Name CHAR(25) NOT NULL,
            Last_Name CHAR(25),
            Score INT
         );"""
-cursor.execute(table)
-#data1 = """INSERT INTO TABLEZ(Email, First_Name, Last_Name, Score) VALUES ("pinkjolly333@gmail.com", 'Pia', 'Jolly', 0);"""
-cursor.execute("""INSERT INTO TABLEZ(Email, First_Name, Last_Name, Score) VALUES ("pinkjolly333@gmail.com", 'Pia', 'Jolly', 0);""")
+#cursor.execute(table)
+cursor.execute("""SELECT * FROM TABLEZ ORDER BY Score DESC""")
+print(cursor.fetchall())
 connection.commit()
 print("heart")
-
-
-
-
-
-
-
 
 
 
