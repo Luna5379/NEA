2d9d9bc37e95a5a32f4e02622c05481bfe9d75bd
Silvia Santamaria <silviasantabuil@outlook.com>
2025-01-27 12:47:04 +0000

    added first 2 lesson questions

--- lessons code.py
+++ lessons code.py
@@ -2,14 +2,19 @@ import sqlite3
 
 connection = sqlite3.connect('lessons.db')
 cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS LESSONS")
+#cursor.execute("DROP TABLE IF EXISTS LESSONS")
 table = """CREATE TABLE LESSONS (
            Question VARCHAR(255) NOT NULL,
            Answer VARCHAR(255) NOT NULL,
            Difficulty INT,
            Type VARCHAR(25),
            LessonPercentage REAL,
-           Options LIST,
+           Options VARCHAR(255),
            Example LONGTEXT
         );"""
-cursor.execute(table)
+#cursor.execute(table)
+cursor.execute("""INSERT INTO LESSONS (Question, Answer, Difficulty, Type, LessonPercentage, Options, Example)
+               VALUES ('What does the input(x) command do?', 'Outputs x and allows the user to input data', '1', 'What does this command do?', 0.05, 'Asks user to input x,Sets input to x,Outputs x', 'eg. input("What is your name?")');""")
+cursor.execute("""SELECT * FROM LESSONS""")
+print(cursor.fetchall())
+connection.commit()
